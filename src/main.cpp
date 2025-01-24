50e09136420ce6646dc6eacf6d53684ac40938b7
Ninjune <enderknight537@gmail.com>
2025-01-24 15:46:16 -0600

    Project setup

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+﻿// Rolling Thunder.cpp : Defines the entry point for the application.
+//
+#include "main.h"
+#include <SFML/Graphics.hpp>
+
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+
+	return 0;
+}
