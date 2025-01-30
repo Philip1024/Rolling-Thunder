3e113184fed07e5eb6c48e9d7bbb5f998e2a7a4d
26pac01 <26pac01@stu.cpsk12.org>
2025-01-30 11:02:36 -0600

    Update

--- src/Entity/Entity.h
+++ src/Entity/Entity.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include<SFML/Graphics.hpp>
 
 
 class Entity
@@ -8,6 +9,8 @@ public:
 	Entity();
 	static std::vector<Entity*>& getEntities() { return entities; }
 	virtual void collide(Entity* other) = 0;
+	std::optional<sf::Sprite> sprite;
+	sf::Texture* texture;
 private:
 	static std::vector<Entity*> entities;
 };
---
+++ src/Entity/Player/Player.h
@@ -0,0 +1,11 @@
+#pragma once
+#include<SFML/Graphics.hpp>
+#include<optional>
+class Player
+{
+	std::optional<sf::Sprite> sprite;
+
+
+
+};
+
