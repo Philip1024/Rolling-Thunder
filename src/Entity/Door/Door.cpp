62f8c88a2e28caca46f07a3f4f21546ea10e6469
Tclervi <treyclervi123@gmail.com>
2025-02-05 11:28:51 -0600

    a

--- src/Entity/Door/Door.cpp
+++ src/Entity/Door/Door.cpp
@@ -1,1 +1,21 @@
 #include "Door.h"
+#include"../Entity.h"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include<optional>
+Door::Door()
+{
+	texture.loadFromFile("res/SpriteSheets/Door.png");
+	sprite.emplace(texture);
+	sprite->setPosition(sf::Vector2f(200, 67));
+	sf::Vector2i position(200, 58);
+	sf::Vector2i size(30, 60);
+	sf::IntRect frame(position, size);
+	sprite->setTextureRect(frame);
+}
+
+
+void Door::open()
+{
+	//
+}
--- src/Entity/Door/Door.h
+++ src/Entity/Door/Door.h
@@ -1,5 +1,19 @@
 #pragma once
-class Door
+#include"../Entity.h"
+
+
+class Door:public Entity
+
 {
+public:
+	Door();
+	~Door();
+	//represents position on sprite sheet
+    //updates player sprite based on input
+	int xPos, yPos;
+	//this opens the door if the player interacts with it 
+	void open();
+
+private:
 };
 
