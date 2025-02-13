bf2df202568fd71d49f8a16e2fb13ef5bed9b1f7
Tclervi <treyclervi123@gmail.com>
2025-02-13 10:54:09 -0600

    aa

--- src/Entity/Enemy/Shooter/Shooter.cpp
+++ src/Entity/Enemy/Shooter/Shooter.cpp
@@ -2,22 +2,3 @@
 #include"../../Player/Player.h"
 #include"../../Bullet/Bullet.h"
 
-//When this collies with 
-void Shooter::collide(Entity* other)
-{
-	Player* playerCast = dynamic_cast<Player*>(other);
-
-	//if they collide set the player bounce bool to true
-	//then set shooter bounce bool to true.
-	if (playerCast != nullptr)
-	{
-		playerCast->update();
-	}
-
-	Bullet* bulletCast = dynamic_cast<Bullet*>(other);
-
-	if (bulletCast != nullptr)
-	{
-		bulletCast->open();
-	}
-}
