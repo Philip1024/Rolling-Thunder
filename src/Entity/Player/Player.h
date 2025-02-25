d767b1aab9813eeec199ea59617f810f83a58924
26pac01 <26pac01@stu.cpsk12.org>
2025-02-25 11:55:24 -0600

    update

--- src/Entity/Player/Player.h
+++ src/Entity/Player/Player.h
@@ -3,14 +3,15 @@
 #include "../../SpriteData/AnimationData.h"
 #include "../Entity.h"
 #include "../Door/Door.h"
-
+#include<vector>
 class Player : public Entity
 {
 public:
 	Player();
 	~Player();
 	void collide(Entity* other) override;
-	void update(char actionFlags) override;
+	void update(char actionFlags,std::vector<sf::FloatRect>);
+	//sf::Vector2f getPosition();
 private:
 	//keeps track of time between sprite updates
 	sf::Clock clock;
@@ -19,5 +20,6 @@ private:
 	bool heightJumped;
 	int animationFrame, jumpFrame;
 	float xMov, yMov,xPos,yPos;//used to keep track of movement of sprite in jumping animation
+	const double PI = 3.14159235;
 };
 
