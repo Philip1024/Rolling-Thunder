2fe15f4dbce9af1290b42e601d4e957905b8d937
Ninjune <enderknight537@gmail.com>
2025-05-09 16:00:42 -0500

    add comment at top of main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,28 @@
 ﻿/*
-Rolling Thunder
+Rolling Thunder (https://github.com/Ninjune/Rolling-Thunder)
+An arcade game
+
+Authors:
+
+Keymap:
+(Menu)
+5 - Add credit
+6 - Player one button
+
+(Ingame)
+W - Enter door/hold when jumping to get on rails
+D - Move right
+A - Move left
+F - Jump
+R - Shoot
+
+What needs work:
+- more animations
+- enemy recolors/other enemies
+- other levels
+- UI needs finished/polishing
+
+We did not cheat.
 */
 #include <SFML/Graphics.hpp>
 #include "Game/Game.h"
